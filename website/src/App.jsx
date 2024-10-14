9ade186fb7615133315d9ab1849593670cb7bfa5
Thiago <thberenger27@gmail.com>
2024-10-14 14:00:35 -0300

    criando componentes iniciais

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+
 function App() {
   return (
     <div className='App'>
-      <h2>Header</h2>
-      <Outlet/>
-      <h2>Footer</h2>
+      <Header />
+        <Outlet />
+      <Footer />
     </div>
   )
 }
