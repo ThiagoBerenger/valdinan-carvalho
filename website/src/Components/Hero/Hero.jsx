f0960601369bad2e7a2c102a89649f4efabb050c
Thiago <thberenger27@gmail.com>
2024-10-14 20:57:00 -0300

    adicionando a hero section

--- website/src/Components/Hero/Hero.jsx
+++ website/src/Components/Hero/Hero.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Hero.modules.css'
 
+import heroImage from '../../assets/juiz.jpg'
+
+import { FaWhatsapp } from "react-icons/fa";
+
 const Hero = () => {
   return (
-    <div>
-        <h1>Valdinan Carvalho</h1>
+    <div className='hero-container'>
+      <img src={heroImage} alt="Valdinan Carvalho - Advocacia e Consultoria Jurídica" loading='lazy'/>
+        <div className="hero-content">
+          <h1>Valdinan Carvalho Advocacia & Consultoria Juridica</h1>
+          <h2>Transformando desafios jurídicos complexos em soluções eficazes e personalizadas</h2>
+          <button><FaWhatsapp /> Entre em contato</button>
+        </div>
     </div>
   )
 }
