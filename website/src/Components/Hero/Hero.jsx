aed107b442ff053ee0831c271a9e7e0e7b61a2da
Thiago <thberenger27@gmail.com>
2024-10-14 21:42:09 -0300

    adicionando link ao whatsapp

--- website/src/Components/Hero/Hero.jsx
+++ website/src/Components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <div className="hero-content">
           <h1>Valdinan Carvalho Advocacia & Consultoria Juridica</h1>
           <h2>Transformando desafios jurídicos complexos em soluções eficazes e personalizadas</h2>
-          <button><FaWhatsapp /> Entre em contato</button>
+          <a href='https://api.whatsapp.com/send?phone=5521994634084&text=Ol%C3%A1!%20Preciso%20de%20um%20advogado.%20Pode%20me%20dar%20mais%20informa%C3%A7%C3%B5es%2C%20por%20favor%3F' target='blank'><FaWhatsapp /> Entre em contato</a>
         </div>
     </div>
   )
