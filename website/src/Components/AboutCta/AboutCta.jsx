c6bcefc034256983dc6c373151f0822bd6b549c4
Thiago <thberenger27@gmail.com>
2024-10-15 00:41:49 -0300

    adicionando call to action para aba sobre

---
+++ website/src/Components/AboutCta/AboutCta.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './AboutCta.modules.css'
+
+const AboutCta = () => {
+  return (
+    <div className='about-cta_container'>
+        <h2>CONHEÇA A NOSSA HISTÓRIA</h2>
+        <h3>Valdinan Carvalho Advocacia & Consultoria Jurídica</h3>
+        <p>Fundado com o objetivo de oferecer serviços jurídicos de excelência, nosso escritório de advocacia alia seriedade, competência, compromisso com prazos, empatia e confiança para atender às necessidades...</p>
+        <a>Saiba mais!</a>
+    </div>
+  )
+}
+
+export default AboutCta
--- website/src/Routes/Home.jsx
+++ website/src/Routes/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import ServicesComponent from '../Components/ServicesComponent/ServicesComponent'
+import AboutCta from '../Components/AboutCTA/AboutCTA'
 
 const Home = () => {
   return (
     <div>
         <Hero />
         <ServicesComponent/>
+        <AboutCta/>
     </div>
   )
 }
