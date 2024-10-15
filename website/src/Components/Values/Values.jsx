d4f00999721a52ed0a3bd45ae499a20898a87763
Thiago <thberenger27@gmail.com>
2024-10-15 01:50:38 -0300

    adicionando aba de valores

---
+++ website/src/Components/Values/Values.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Values.modules.css';
+
+import { PiHandshake, PiMedal, PiLightbulb, PiUsers, PiHeart, PiGlobe, PiCheckCircle } from 'react-icons/pi';
+
+const Values = () => {
+  return (
+    <div className='values-container'>
+      <h2>Nossos Valores</h2>
+      <div className="values-boxes_container">
+        <div className="value-box">
+          <PiHandshake size={40} />
+          <h3>Ética</h3>
+        </div>
+
+        <div className="value-box">
+          <PiMedal size={40} />
+          <h3>Excelência Técnica</h3>
+        </div>
+
+        <div className="value-box">
+          <PiLightbulb size={40} />
+          <h3>Inovação</h3>
+        </div>
+
+        <div className="value-box">
+          <PiUsers size={40} />
+          <h3>Trabalho em Equipe</h3>
+        </div>
+
+        <div className="value-box">
+          <PiHeart size={40} />
+          <h3>Empatia</h3>
+        </div>
+
+        <div className="value-box">
+          <PiGlobe size={40} />
+          <h3>Responsabilidade Social</h3>
+        </div>
+
+        <div className="value-box">
+          <PiCheckCircle size={40} />
+          <h3>Compliance</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Values;
--- website/src/Routes/About.jsx
+++ website/src/Routes/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutPage from '../Components/AboutPage/AboutPage'
+import Values from '../Components/Values/Values'
 
 const About = () => {
   return (
     <div>
-      <AboutPage/>
+      <AboutPage />
+      <Values />
     </div>
   )
 }
