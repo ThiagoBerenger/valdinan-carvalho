3c871ea51fed16e4b476f990006703d87b8e1954
Thiago <thberenger27@gmail.com>
2024-10-17 19:32:12 -0300

    adicionando animação na home

--- website/src/Components/Hero/Hero.jsx
+++ website/src/Components/Hero/Hero.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Hero.modules.css'
 
+// animação scroll
+import { motion } from "framer-motion"
+
 import heroImage from '../../assets/juiz.png'
 
 import { FaWhatsapp } from "react-icons/fa";
@@ -9,11 +12,17 @@ const Hero = () => {
   return (
     <div className='hero-container'>
       <img src={heroImage} alt="Valdinan Carvalho - Advocacia e Consultoria Jurídica" loading='lazy'/>
-        <div className="hero-content">
+
+        <motion.div className="hero-content"
+           initial={{ x: '-20vw', opacity: 0 }} 
+           animate={{ x: 0, opacity: 1}}
+           transition={{ type: "spring", stiffness: 100, damping: 20 }}
+        >
           <h1>Valdinan Carvalho Advocacia & Consultoria Juridica</h1>
           <h2>Transformando desafios jurídicos complexos em soluções eficazes e personalizadas</h2>
           <a href='https://api.whatsapp.com/send?phone=5521994634084&text=Ol%C3%A1!%20Preciso%20de%20um%20advogado.%20Pode%20me%20dar%20mais%20informa%C3%A7%C3%B5es%2C%20por%20favor%3F' target='blank'><FaWhatsapp /> Entre em contato</a>
-        </div>
+        </motion.div>
+        
     </div>
   )
 }
