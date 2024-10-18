a09928f5745f97cba14e843718d10df5c1d94454
Thiago <thberenger27@gmail.com>
2024-10-18 17:20:04 -0300

    ajustando header

--- website/src/Components/Header/Header.jsx
+++ website/src/Components/Header/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
         <Link to='/'><img src={logo} alt="Logo Valdinan Carvalho - Advocacia e Consultoria Jurídica" className="logo" /></Link>
         
         {/* Menu hambúrguer */}
-        <button className="hamburger" onClick={toggleMenu}>
+        <button className={`hamburger ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
           <div></div>
           <div></div>
           <div></div>
@@ -34,26 +34,26 @@ const Header = () => {
         <nav className="nav">
           {/* Menu de navegação */}
           <ul className={`nav-list ${menuOpen ? 'active' : ''}`}>
-            <li className="nav-item"><Link to="/">Home</Link></li>
-            <li className="nav-item"><Link to="/sobre">Sobre</Link></li>
+            <li className="nav-item"><Link to="/" onClick={() => {setMenuOpen(false)}}>Home</Link></li>
+            <li className="nav-item"><Link to="/sobre" onClick={() => {setMenuOpen(false)}}>Sobre</Link></li>
 
             {/* Dropdown para Área de Atuação */}
             <li className="nav-item" onClick={toggleDropdown} aria-haspopup="true" aria-expanded={dropdownOpen ? "true" : "false"}>
               <a href="#area-de-atuacao"><IoMdArrowDropdown/> Área de Atuação</a>
               {dropdownOpen && (
                 <ul className="dropdown">
-                  <li className="dropdown-item"><Link to="/direito-civil">Civil</Link></li>
-                  <li className="dropdown-item"><Link to="/direito-penal">Penal</Link></li>
-                  <li className="dropdown-item"><Link to="/direito-trabalhista">Trabalhista</Link></li>
-                  <li className="dropdown-item"><Link to="/direito-empresarial">Empresarial</Link></li>
-                  <li className="dropdown-item"><Link to="/direito-da-familia">Família</Link></li>
-                  <li className="dropdown-item"><Link to="/direito-tributario">Tributário</Link></li>
-                  <li className="dropdown-item"><Link to="/direito-do-consumidor">Consumidor</Link></li>
-                  <li className="dropdown-item"><Link to="/direito-previdenciario">Previdenciário</Link></li>
+                  <li className="dropdown-item"><Link to="/direito-civil" onClick={() => {setMenuOpen(false)}}>Civil</Link></li>
+                  <li className="dropdown-item"><Link to="/direito-penal" onClick={() => {setMenuOpen(false)}}>Penal</Link></li>
+                  <li className="dropdown-item"><Link to="/direito-trabalhista" onClick={() => {setMenuOpen(false)}}>Trabalhista</Link></li>
+                  <li className="dropdown-item"><Link to="/direito-empresarial" onClick={() => {setMenuOpen(false)}}>Empresarial</Link></li>
+                  <li className="dropdown-item"><Link to="/direito-da-familia" onClick={() => {setMenuOpen(false)}}>Família</Link></li>
+                  <li className="dropdown-item"><Link to="/direito-tributario" onClick={() => {setMenuOpen(false)}}>Tributário</Link></li>
+                  <li className="dropdown-item"><Link to="/direito-do-consumidor" onClick={() => {setMenuOpen(false)}}>Consumidor</Link></li>
+                  <li className="dropdown-item"><Link to="/direito-previdenciario" onClick={() => {setMenuOpen(false)}}>Previdenciário</Link></li>
                 </ul>
               )}
             </li>
-            <li className="nav-item"><Link to="/contato">Contato</Link></li>
+            <li className="nav-item"><Link to="/contato" onClick={() => {setMenuOpen(false)}}>Contato</Link></li>
           </ul>
         </nav>
       </div>
