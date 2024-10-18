80ee56a0f3ee99d9204abb33eec0ae21baee4fa6
Thiago <thberenger27@gmail.com>
2024-10-18 17:38:51 -0300

    adicionando animação ao areas component

--- website/src/Components/AreasComponent/AreasComponent.jsx
+++ website/src/Components/AreasComponent/AreasComponent.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './AreasComponent.modules.css'
 
 import areaImage from '../../assets/backgroundPilares.jpg'
 import escritorio from '../../assets/escritorio.jpg'
 
+// animação
+import { motion, useInView } from 'framer-motion'
 
 const AreasComponent = ({title, text}) => {
 
+  const ref = useRef(null)
+  const isInView = useInView(ref, {once: true})
+
     const TextWithLineBreaks = ({ text }) => {
         return (
-          <div>
+          <motion.div
+          ref={ref}
+          initial={{ x: '-20vw', opacity: 0 }} 
+          animate={isInView ? { x: 0, opacity: 1} : { x: '-20vw', opacity: 0 }}
+          transition={{ type: "spring", stiffness: 100, damping: 20 }}
+          >
             {text.split('\n').map((line, index) => (
               <React.Fragment key={index}>
                 {line}
                 <br />
               </React.Fragment>
             ))}
-          </div>
+          </motion.div>
         );
       };
 
