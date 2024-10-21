39b70fcd8a1906e5f5227127599c5fe426fa8054
Thiago <thberenger27@gmail.com>
2024-10-21 14:50:26 -0300

    ajustando import do call to action

--- website/src/Routes/Home.jsx
+++ website/src/Routes/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import ServicesComponent from '../Components/ServicesComponent/ServicesComponent'
-import AboutCta from '../Components/AboutCTA/AboutCTA'
+import AboutCta from '../Components/AboutCta/AboutCta'
 import Form from '../Components/Form/Form'
 import GoogleReviews from '../Components/GoogleReviews/GoogleReviews'
 
