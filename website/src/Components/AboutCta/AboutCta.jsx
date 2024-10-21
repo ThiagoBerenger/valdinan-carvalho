69250f2ae17d241a4d6e4682d7cc52b9293e47be
Thiago <thberenger27@gmail.com>
2024-10-21 13:28:51 -0300

    adicionando redirecionamento about-cta

--- website/src/Components/AboutCta/AboutCta.jsx
+++ website/src/Components/AboutCta/AboutCta.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import './AboutCta.modules.css'
 
 const AboutCta = () => {
@@ -7,7 +9,7 @@ const AboutCta = () => {
         <h2>CONHEÇA A NOSSA HISTÓRIA</h2>
         <h3>Valdinan Carvalho Advocacia & Consultoria Jurídica</h3>
         <p>Fundado com o objetivo de oferecer serviços jurídicos de excelência, nosso escritório de advocacia alia seriedade, competência, compromisso com prazos, empatia e confiança para atender às necessidades...</p>
-        <a>Saiba mais!</a>
+        <Link to='/sobre'>Saiba mais!</Link>
     </div>
   )
 }
