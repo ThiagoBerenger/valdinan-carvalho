639acb55a791b4714abf0442e2d80fa0593d7b69
Thiago <thberenger27@gmail.com>
2024-10-30 16:09:59 -0300

    atualizando cta about

--- website/src/Components/AboutCta/AboutCta.jsx
+++ website/src/Components/AboutCta/AboutCta.jsx
@@ -8,7 +8,7 @@ const AboutCta = () => {
     <div className='about-cta_container'>
         <h2>CONHEÇA A NOSSA HISTÓRIA</h2>
         <h3>Valdinan Carvalho Advocacia & Consultoria Jurídica</h3>
-        <p>Fundado com o objetivo de oferecer serviços jurídicos de excelência, nosso escritório de advocacia alia seriedade, competência, compromisso com prazos, empatia e confiança para atender às necessidades...</p>
+        <p>Idealizado com o objetivo de oferecer serviços jurídicos de excelência, nosso escritório de advocacia alia seriedade, competência, compromisso com prazos, empatia e confiança para atender às necessidades...</p>
         <Link to='/sobre'>Saiba mais!</Link>
     </div>
   )
