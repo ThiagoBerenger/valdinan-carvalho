83822f39416d54df9577e0d8d561c4ff85af4503
Thiago <thberenger27@gmail.com>
2024-11-06 16:18:49 -0300

    instalando sitemap

---
+++ website/sitemap-generator.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const router = require('./src/App');  // Caminho para o seu arquivo de rotas (geralmente o App.js)
+const Sitemap = require('react-router-sitemap').default;
+
+// Gerar o sitemap
+(new Sitemap(router))
+  .build('https://www.valdinancarvalho.com.br') // Substitua pelo seu domínio
+  .save(path.resolve(__dirname, 'public', 'sitemap.xml')); // O local do arquivo sitemap.xml
